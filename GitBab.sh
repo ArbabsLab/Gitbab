e09830fe5df4d081d983baca173fc8d85133ee9a
Arbab Husain <arbabhusain200@gmail.com>
2024-12-30 23:31:43 -0500

    [any message]

---
+++ GitBab.sh
@@ -0,0 +1,3 @@
+set -e 
+
+PYTHONPATH=$(dirname $0) exec python3 -m app.main "$@"
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import zlib
 
 def main():
     command = sys.argv[1]
@@ -11,9 +12,16 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+    elif command == "cat-file" and sys.argv[2] == "-p":
+        blob = sys.argv[3]
+        with open(f".git/objects/{blob[0:2]}/{blob[2:]}", "rb") as f:
+            blob_data = zlib.decompress(f.read()).decode(encoding="utf-8")
+        print(blob_data.split("\0")[1])
+
     else:
         raise RuntimeError(f"Unknown command #{command}")
     
 
 if __name__ == "__main__":
     main()
+
